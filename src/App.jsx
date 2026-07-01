4f86430e11aae2ca1672e5b5d91d0fcf375bc1ce
Basilraju-10 <basilraju010@gmail.com>
2026-07-01 12:31:49 +0530

    feat: implement state selection and capital display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./components/Card"
 
 
- export default function App() {
-  const numberRef = useRef();
-  const [fact,setFact] = useState();
-  const [loading,setLoading] = useState(false);
 
-  const getFact = async() => {
-    setLoading(true)
-    const number = numberRef.current.value
-const response = await fetch('https://catfact.ninja/fact')
-    const text = await response.text()
-    setLoading(false)
-    setFact(text)
-  }
-  if(loading){
-    return <div>Loading...</div>
-  }
+ export default function App() {
+const data = {
+  kerala : "thiruvanathapuram",
+  karanataka : "bengaluru",
+  tamilnadu : "chennai",
+}
+const [selectedState, setSelectedState] = useState("Kerala")
+const [selectedStateCapital, setSelectedStateCaptial] = useState("Trivanathapuram")
+const changeState =(e) => {
+    setSelexportectedState(e.target.value);
+}
+const changeCapital = () =>{
+    setSelectedStateCaptial(data[selectedState]);
+}
+useEffect(changeCapital,[selectedState])
+
   return (
     <div>
-      <input ref={numberRef} type="number" placeholder="Enter Number"/>
-      <button onClick={getFact}>Get Fact</button>
-      <p>{fact}</p>
+      <select onChange = {changeState}>
+        <option value="kerala"> Kerala</option>
+        <option value="karanataka">Karnataka</option>
+        <option value="tamilnadu">Tamilnadu</option>
+      </select>
+      <p>"selected state is: {selectedState}"</p>
+      <p>"selected state capital is: {selectedStateCapital}"</p>
+      
+      
     </div>
   )
 }
